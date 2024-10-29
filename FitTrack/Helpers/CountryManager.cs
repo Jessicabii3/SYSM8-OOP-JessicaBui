046cbdeeddfe49c90df4e09cfb57ed232ff9541c
Jessicabii3 <159479108+Jessicabii3@users.noreply.github.com>
2024-10-29 04:37:56 +0100

    CountryManager för att hämta och sortera en lista med länder

---
+++ FitTrack/Helpers/CountryManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack.Helpers
+{
+    public class CountryManager
+    {
+        // Hämtar en lista med alla tillgängliga länder, sorterade i alfabetisk ordning
+        public static ObservableCollection<string> GetCountries()
+        {
+            var countries = CultureInfo.GetCultures(CultureTypes.SpecificCultures)
+                .Select(culture => new RegionInfo(culture.Name).DisplayName)
+                .Distinct() // Tar bort dubbletter
+                .OrderBy(country => country) // Sorterar länder i alfabetisk ordning
+                .ToList();
+
+            return new ObservableCollection<string>(countries); // Returnerar listan som en ObservableCollection
+        }
+    }
+}
