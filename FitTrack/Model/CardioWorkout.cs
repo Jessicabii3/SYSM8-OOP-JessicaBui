6d17d7469c22a50736cdac9a30cbbc4fd0c74ccc
Jessicabii3 <159479108+Jessicabii3@users.noreply.github.com>
2024-10-29 02:56:20 +0100

    CardioWorkout-klass med specifik kaloriberäkning

---
+++ FitTrack/Model/CardioWorkout.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack.Model
+{
+    public class CardioWorkout: Workout
+    {
+        //Egenskap
+        public int Distance { get; set; }
+        //Konstruktor
+        public CardioWorkout(DateTime date, TimeSpan duration, int distance, string notes, string owner)
+           : base(date, "Cardio", duration, notes, owner)
+        {
+            Distance = distance;
+        }
+        public override int CalculateCaloriesBurned()
+        {
+            // Beräkning av kalorier baserat på distans och tid
+            CaloriesBurned = (int)(Distance * 0.75 * Duration.TotalMinutes);
+            return CaloriesBurned;
+        }
+    }
+}
