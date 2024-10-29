a4844201a62d8b98189c72dc568c4dfadc7a3b4c
Jessicabii3 <159479108+Jessicabii3@users.noreply.github.com>
2024-10-29 02:55:07 +0100

    bas-klassen Workout med egenskaper och kaloriberäkning

---
+++ FitTrack/Model/Workout.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack.Model
+{
+    public abstract class Workout
+    {
+        // Egenskaper för träningspassets datum, typ, varaktighet, brända kalorier och anteckningar
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+        public TimeSpan Duration { get; set; }
+        public int CaloriesBurned { get; set; }
+        public string Notes { get; set; }
+        public string Owner { get; set; }  // Ägaren till träningspasset(användarnamn)
+
+        //Konstruktor
+        public Workout(DateTime date, string type, TimeSpan duration, string notes, string owner)
+        {
+            Date = date;
+            Type = type;
+            Duration = duration;
+            Notes = notes;
+            Owner = owner;
+        }
+        public virtual int CalculateCaloriesBurned()
+        {
+            return CaloriesBurned;
+        }
+        public void UpdateCaloriesBurned(int calories)
+        {
+            CaloriesBurned = calories;
+        }
+    }
+}
