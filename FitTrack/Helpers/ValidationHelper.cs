be6cda33c7c74e03f6b12b837d76593294c907f2
Jessicabii3 <159479108+Jessicabii3@users.noreply.github.com>
2024-10-29 02:48:22 +0100

    Gjort Person med egenskaperna Username, password och metoden SignIn

---
+++ FitTrack/Helpers/ValidationHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FitTrack.Helpers
+{
+    public class ValidationHelper
+    {
+        public static bool IsValidPassword(string password)
+        {
+            if (password.Length < 8) return false;
+            if (!Regex.IsMatch(password, @"\d")) return false; // Minst en siffra
+            if (!Regex.IsMatch(password, @"[\W_]")) return false; // Minst ett specialtecken
+            return true;
+        }
+        public static bool IsValidUsername(string username)
+        {
+            return !string.IsNullOrEmpty(username) && username.Length >= 3;
+        }
+    }
+}
---
+++ FitTrack/Model/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack.Model
+{
+    public abstract class Person
+    {
+        //Egenskaper
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        //Konstruktor
+        public Person(string Username, string Password)
+        {
+            this.Username = Username;
+            this.Password = Password;
+
+        }
+        // Abstrakt metod som måste implementeras av klasser som ärver från Person
+        public abstract void SignIn();
+    }
+}
