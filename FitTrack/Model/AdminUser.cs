21d64baa956a66884e4f39035894cc7ac418c1b8
Jessicabii3 <159479108+Jessicabii3@users.noreply.github.com>
2024-10-29 02:51:11 +0100

    AdminUser-klass med hantering av användares träningspass och användarhämtning

---
+++ FitTrack/Model/AdminUser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack.Model
+{
+    public class AdminUser:User
+    {
+        // Konstruktor
+        public AdminUser(string username, string password, string country, string securityQuestion, string securityAnswer)
+            : base(username, password, country, securityQuestion, securityAnswer, isAdmin: true)
+        {
+        }
+        // Metod för att hämta information om alla användares träningspass
+        public ObservableCollection<UserWorkoutInfo> GetAllWorkoutsInfo(ManageUser manageUser)
+        {
+            // Samlar alla träningspass från alla användare
+            var workoutDetails = new ObservableCollection<UserWorkoutInfo>(
+                manageUser.GetAllUsers()
+                    .SelectMany(user => user.UserWorkouts,
+                                (user, workout) => new UserWorkoutInfo(user.Username, workout.Type, workout.Duration))
+            );
+
+            return workoutDetails;
+        }
+        // Metod för att ta bort ett träningspass för en specifik användare
+        public void RemoveWorkoutForUser(User user, Workout workout)
+        {
+            if (user?.UserWorkouts != null && workout != null)
+            {
+                user.UserWorkouts.Remove(workout);
+            }
+        }
+        // Metod för att hämta en specifik användare baserat på användarnamn
+        public User? GetUser(string username, ManageUser manageUser)
+        {
+            return manageUser.GetUser(username); // Admin använder ManageUser för att hitta användaren
+        }
+    }
+}
